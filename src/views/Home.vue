371d8cdf6fe3849f566d18a02ebc066e66fa26e3
Hazem Gharib <hazem.gharib@servicerocket.com>
2020-09-13 00:21:32 +0800

    Fix players IDs in Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
-    <div v-if="!currentEvent && !nextEvent" class="ma-16 text-center">
+    <div
+      v-if="!currentEvent && !nextEvent && !players"
+      class="ma-16 text-center"
+    >
       <v-progress-circular
         :size="100"
         :width="10"
@@ -8,7 +11,7 @@
         indeterminate
       ></v-progress-circular>
     </div>
-    <v-container class="mt-8 mb-16" v-if="currentEvent && currentEvent.length">
+    <div class="mt-8 mb-16" v-if="currentEvent && currentEvent.length">
       <div class="font-weight-black text-h4 text-center">Current Event</div>
       <div class="ma-8" v-for="i in currentEvent" :key="i.name">
         <v-card class="pa-4 text-center">
@@ -19,15 +22,51 @@
             {{ new Date(i.deadline_time).toLocaleDateString("en-my") }}
           </div>
           <div>Average entry score: {{ i.average_entry_score }}</div>
-          <div>Highest score: {{ i.highest_score || "-" }}</div>
-          <div>Most captained: {{ i.most_captained || "-" }}</div>
-          <div>Most selected: {{ i.most_selected || "-" }}</div>
-          <div>Most vice_captained: {{ i.most_vice_captained || "-" }}</div>
-          <div>Top element: {{ i.top_element || "-" }}</div>
+          <div>Highest score: {{ i.highest_score || "N/A" }}</div>
+          <div>
+            Most selected:
+            {{
+              i.most_selected
+                ? `${getPlayer(i.most_selected).first_name} ${
+                    getPlayer(i.most_selected).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
+          <div>
+            Most captained:
+            {{
+              i.most_captained
+                ? `${getPlayer(i.most_captained).first_name} ${
+                    getPlayer(i.most_captained).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
+          <div>
+            Most vice captained:
+            {{
+              i.most_vice_captained
+                ? `${getPlayer(i.most_vice_captained).first_name} ${
+                    getPlayer(i.most_vice_captained).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
+          <div>
+            Top Player:
+            {{
+              i.top_element
+                ? `${getPlayer(i.top_element).first_name} ${
+                    getPlayer(i.top_element).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
         </v-card>
       </div>
-    </v-container>
-    <v-container class="mt-8 mb-16" v-if="nextEvent && nextEvent.length">
+    </div>
+    <div class="mt-8 mb-16" v-if="nextEvent && nextEvent.length">
       <div class="font-weight-black text-h4 text-center">Next Event</div>
       <div class="ma-8" v-for="i in nextEvent" :key="i.name">
         <v-card class="pa-4 text-center">
@@ -38,15 +77,51 @@
             {{ new Date(i.deadline_time).toLocaleDateString("en-my") }}
           </div>
           <div>Average entry score: {{ i.average_entry_score }}</div>
-          <div>Highest score: {{ i.highest_score || "-" }}</div>
-          <div>Most captained: {{ i.most_captained || "-" }}</div>
-          <div>Most selected: {{ i.most_selected || "-" }}</div>
-          <div>Most vice_captained: {{ i.most_vice_captained || "-" }}</div>
-          <div>Top element: {{ i.top_element || "-" }}</div>
+          <div>Highest score: {{ i.highest_score || "N/A" }}</div>
+          <div>
+            Most selected:
+            {{
+              i.most_selected
+                ? `${getPlayer(i.most_selected).first_name} ${
+                    getPlayer(i.most_selected).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
+          <div>
+            Most captained:
+            {{
+              i.most_captained
+                ? `${getPlayer(i.most_captained).first_name} ${
+                    getPlayer(i.most_captained).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
+          <div>
+            Most vice_captained:
+            {{
+              i.most_vice_captained
+                ? `${getPlayer(i.most_vice_captained).first_name} ${
+                    getPlayer(i.most_vice_captained).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
+          <div>
+            Top Player:
+            {{
+              i.top_element
+                ? `${getPlayer(i.top_element).first_name} ${
+                    getPlayer(i.top_element).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
         </v-card>
       </div>
-    </v-container>
-    <v-container class="mt-8 mb-16" v-if="lastEvent && lastEvent.length">
+    </div>
+    <div class="mt-8 mb-16" v-if="lastEvent && lastEvent.length">
       <div class="font-weight-black text-h4 text-center">Last Event</div>
       <div class="ma-8" v-for="i in lastEvent" :key="i.name">
         <v-card class="pa-4 text-center">
@@ -57,31 +132,75 @@
             {{ new Date(i.deadline_time).toLocaleDateString("en-my") }}
           </div>
           <div>Average entry score: {{ i.average_entry_score }}</div>
-          <div>Highest score: {{ i.highest_score || "-" }}</div>
-          <div>Most captained: {{ i.most_captained || "-" }}</div>
-          <div>Most selected: {{ i.most_selected || "-" }}</div>
-          <div>Most vice_captained: {{ i.most_vice_captained || "-" }}</div>
-          <div>Top element: {{ i.top_element || "-" }}</div>
+          <div>Highest score: {{ i.highest_score || "N/A" }}</div>
+          <div>
+            Most selected:
+            {{
+              i.most_selected
+                ? `${getPlayer(i.most_selected).first_name} ${
+                    getPlayer(i.most_selected).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
+          <div>
+            Most captained:
+            {{
+              i.most_captained
+                ? `${getPlayer(i.most_captained).first_name} ${
+                    getPlayer(i.most_captained).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
+          <div>
+            Most vice captained:
+            {{
+              i.most_vice_captained
+                ? `${getPlayer(i.most_vice_captained).first_name} ${
+                    getPlayer(i.most_vice_captained).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
+          <div>
+            Top Player:
+            {{
+              i.top_element
+                ? `${getPlayer(i.top_element).first_name} ${
+                    getPlayer(i.top_element).last_name
+                  }`
+                : "N/A"
+            }}
+          </div>
         </v-card>
       </div>
-    </v-container>
+    </div>
   </div>
 </template>
 
 <script>
-import { getHome } from "@/services/homeService";
+import { getEvents } from "@/services/eventsService";
+import { getPlayers } from "@/services/playersHubService";
 import { getToken } from "@/services/tokenService";
 export default {
   name: "Home",
   components: {},
   data: () => ({
+    players: undefined,
     currentEvent: undefined,
     nextEvent: undefined,
     lastEvent: undefined
   }),
   mounted() {
     getToken().then(({ data }) => {
-      getHome(data.token).then(({ data }) => {
+      getPlayers(data.token).then(({ data }) => {
+        this.players = data.results;
+      });
+    });
+
+    getToken().then(({ data }) => {
+      getEvents(data.token).then(({ data }) => {
         this.currentEvent = data?.results.filter(r => r.is_current);
         this.nextEvent = data?.results.filter(r => r.is_next);
 
@@ -93,6 +212,11 @@ export default {
         this.lastEvent = lastEvent ? [this.lastEvent] : [];
       });
     });
+  },
+  methods: {
+    getPlayer(id) {
+      return this.players.find(p => p.id === id);
+    }
   }
 };
 </script>
