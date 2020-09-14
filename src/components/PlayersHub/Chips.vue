596c100825e81a7e5adb625f9c0835decb0df618
Hazem Gharib <hazem.gharib@servicerocket.com>
2020-09-14 17:12:20 +0800

    Implement subs and and chips

---
+++ src/components/PlayersHub/Chips.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-chip
+      class="ma-2"
+      :color="chip.status_for_entry === 'available' ? 'success' : 'danger'"
+      link
+      v-for="chip in chips"
+      :key="chip.name"
+    >
+      {{ getChipName(chip.name) }}
+    </v-chip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chips",
+  props: {
+    chips: {
+      required: true
+    }
+  },
+  methods: {
+    getChipName(chip) {
+      switch (chip) {
+        case "wildcard":
+          return "Wildcard";
+        case "freehit":
+          return "Free Hit";
+        case "bboost":
+          return "Bench Boost";
+        case "3xc":
+          return "Triple Captain";
+        default:
+          break;
+      }
+    }
+  }
+};
+</script>
--- src/components/PlayersHub/FantasyTeamCard.vue
+++ src/components/PlayersHub/FantasyTeamCard.vue
@@ -10,16 +10,54 @@
     </div>
     <v-card v-if="team">
       <v-container>
-        <v-img class="pitch" :src="require('@/assets/pitch.svg')" />
-        <div>
+        <v-img
+          aspect-ratio="1.2"
+          min-height="700px"
+          class="pitch"
+          :src="require('@/assets/pitch.svg')"
+        />
+        <div class="starting">
           <div
-            v-for="player in team.picks"
+            v-for="player in team.picks.filter(p => p.position < 12)"
             :key="player.element"
             :class="`pos-${player.position}`"
           >
-            <v-img :src="getPlayer(player.element).team_shirt" width="50" />
-            <div class="placeholder">{{ getPlayer(player.element).name }}</div>
+            <v-img
+              class="shirt"
+              :src="
+                [1].includes(player.position)
+                  ? getPlayer(player.element).team_shirt_gk
+                  : getPlayer(player.element).team_shirt
+              "
+            />
+            <div class="placeholder">
+              {{ getPlayer(player.element).name }}
+            </div>
           </div>
+        </div>
+        <div class="subs" style="height: 10rem">
+          <div class="text-h6">Substitutes</div>
+          <div
+            v-for="player in team.picks.filter(p => p.position >= 12)"
+            :key="player.element"
+            :class="`pos-${player.position}`"
+          >
+            <v-img
+              class="shirt"
+              :src="
+                [12].includes(player.position)
+                  ? getPlayer(player.element).team_shirt_gk
+                  : getPlayer(player.element).team_shirt
+              "
+            />
+            <div class="placeholder">
+              {{ getPlayer(player.element).name }}
+            </div>
+          </div>
+        </div>
+        <div class="mt-8">
+          <div class="text-h6">Chips</div>
+          <Chips :chips="team.chips" />
         </div>
       </v-container>
     </v-card>
@@ -29,8 +67,12 @@
 <script>
 import { getPlayers, getFantasyTeam } from "@/services/playersHubService";
 import { getToken } from "@/services/tokenService";
+import Chips from "@/components/PlayersHub/Chips";
 export default {
   name: "FantasyTeamCard",
+  components: {
+    Chips
+  },
   data() {
     return {
       players: undefined,
@@ -42,6 +84,20 @@ export default {
     this.getFantasyTeam(process.env.VUE_APP_FANTASY_TEAM_ID);
   },
   methods: {
+    getChipName(chip) {
+      switch (chip) {
+        case "wildcard":
+          return "Wildcard";
+        case "freehit":
+          return "Free Hit";
+        case "bboost":
+          return "Bench Boost";
+        case "3xc":
+          return "Triple Captain";
+        default:
+          break;
+      }
+    },
     getPlayer(id) {
       return this.players.find(p => p.id === id);
     },
@@ -57,7 +113,6 @@ export default {
                 ? p.chance_of_playing_next_round
                 : ""
           }));
-          console.log(this.players);
         });
       });
     },
@@ -65,7 +120,6 @@ export default {
       getToken().then(({ data }) => {
         getFantasyTeam(data.token, team).then(({ data }) => {
           this.team = data.results;
-          console.log(this.team);
         });
       });
     }
@@ -75,13 +129,29 @@ export default {
 <style scoped>
 .placeholder {
   color: #fff;
+  text-align: center;
   background-color: #00000066;
   padding: 0.4rem;
-  margin: 0.4rem -25%;
+  margin: 0.4rem -30%;
   width: fit-content;
   border-radius: 0.4rem;
   font-size: 14px;
   font-weight: bold;
+}
+.shirt {
+  width: 60px;
+}
+@media screen and (max-width: 425px) {
+  .shirt {
+    width: 40px;
+  }
+  .placeholder {
+    font-size: 10px;
+    font-weight: normal;
+    width: min-content;
+    margin: 0.4rem -10%;
+    padding: 4px;
+  }
 }
 .pos-1 {
   position: absolute;
@@ -90,76 +160,80 @@ export default {
 }
 .pos-2 {
   position: absolute;
-  top: 25%;
+  top: 20%;
   left: 20%;
 }
 .pos-3 {
   position: absolute;
-  top: 25%;
+  top: 20%;
   left: 40%;
 }
 .pos-4 {
   position: absolute;
-  top: 25%;
+  top: 20%;
   left: 55%;
 }
 .pos-5 {
   position: absolute;
-  top: 25%;
+  top: 20%;
   left: 75%;
 }
 .pos-6 {
   position: absolute;
-  top: 50%;
+  top: 40%;
   left: 20%;
 }
 .pos-7 {
   position: absolute;
-  top: 50%;
+  top: 40%;
   left: 40%;
 }
 .pos-8 {
   position: absolute;
-  top: 50%;
+  top: 40%;
   left: 55%;
 }
 .pos-9 {
   position: absolute;
-  top: 50%;
+  top: 40%;
   left: 75%;
 }
 .pos-10 {
   position: absolute;
-  top: 75%;
+  top: 55%;
   left: 35%;
 }
 .pos-11 {
   position: absolute;
-  top: 75%;
+  top: 55%;
   left: 60%;
 }
 .pos-12 {
-  position: absolute;
+  /* position: absolute; */
+  float: left;
+  margin: 1rem;
   top: 5%;
   left: 3%;
-  width: 5%;
 }
 .pos-13 {
-  position: absolute;
+  /* position: absolute; */
+  float: left;
+  margin: 1rem;
   top: 30%;
   left: 3%;
-  width: 5%;
 }
 .pos-14 {
-  position: absolute;
+  /* position: absolute; */
+  float: left;
+  margin: 1rem;
   top: 5%;
   left: 92%;
-  width: 5%;
 }
 .pos-15 {
-  position: absolute;
+  /* position: absolute; */
+  float: left;
+  margin: 1rem;
   top: 30%;
   left: 92%;
-  width: 5%;
 }
 </style>
